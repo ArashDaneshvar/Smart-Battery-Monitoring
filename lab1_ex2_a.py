06e1572ed92c91bcf2bf762a147fc665dd7f22d5
Arash Daneshvar <arash.daneshvar.ac@gmail.com>
2023-11-05 23:26:24 +0100

    solution for lab1_ex2_a.py

---
+++ lab1_ex2_a.py
@@ -0,0 +1,8 @@
+import psutil
+
+status = psutil.sensors_battery()
+battery_level = status.percent
+power_is_plugged = status.power_plugged
+
+print("battery_level is: ", battery_level)
+print("power_is_plugged", power_is_plugged)
