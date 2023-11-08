7bf8aed56c0bca2c18b860916ad797bd80ec95c7
ArashDaneshvar <ao.daneshvar@gmail.com>
2023-11-08 13:46:57 +0100

    remove three files from main folder

--- lab1_ex2_a.py
+++
@@ -1,8 +0,0 @@
-import psutil
-
-status = psutil.sensors_battery()
-battery_level = status.percent
-power_is_plugged = status.power_plugged
-
-print("battery_level is: ", battery_level)
-print("power_is_plugged", power_is_plugged)
