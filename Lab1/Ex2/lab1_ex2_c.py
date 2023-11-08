4d661108ebf768dddb668172b6f3d1d8591402c7
ArashDaneshvar <ao.daneshvar@gmail.com>
2023-11-08 14:15:10 +0100

    Upload solutiion files ex2 of lab1

---
+++ Lab1/Ex2/lab1_ex2_c.py
@@ -0,0 +1,20 @@
+import redis 
+
+REDIS_HOST = "redis-14980.c300.eu-central-1-1.ec2.cloud.redislabs.com"
+REDIS_PORT = 14980
+REDIS_USERNAME = "default"
+REDIS_PASSWORD = "WclyL3nHTYtgrBy192F9dEfyWQRrx9gi"
+
+#Connect to redis database
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, username=REDIS_USERNAME, password=REDIS_PASSWORD)
+
+#Check the connection
+is_connect = redis_client.ping()
+print(is_connect)
+
+
+written = redis_client.set("massage", value="Hello World!")
+print("written massage", written)
+
+msg = redis_client.get("massage")
+print(msg.decode())
