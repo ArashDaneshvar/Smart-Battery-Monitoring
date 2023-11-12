e3411f2ab67b40b2703839d27ff8d069bc4f4932
ArashDaneshvar <ao.daneshvar@gmail.com>
2023-11-12 14:41:59 +0100

    modify time problem

--- Lab1/Ex2/lab1_ex2_b.py
+++ Lab1/Ex2/lab1_ex2_b.py
@@ -1,19 +1,19 @@
 import psutil
 import time
 import uuid
-from datetime import date
-
+from datetime import datetime
 
 status = psutil.sensors_battery()
-battery_level = status.percent
-power_is_plugged = status.power_plugged
-
-date_time = date.today().strftime('%Y-%m-%d %H:%M:%S')
 mac_address = hex(uuid.getnode())
 
 while True:
-    print(f'{date_time} - {mac_address}:{battery_level}')
-    print(f'{date_time} - {mac_address}:{power_is_plugged}')
+    timestamp = time.time()
+    battery_level = status.percent
+    power_is_plugged = status.power_plugged
+    date_time_format = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S%f')
+
+    print(f'{date_time_format} - {mac_address}:{battery_level}')
+    print(f'{date_time_format} - {mac_address}:{power_is_plugged}')
     
     time.sleep(2)
 
