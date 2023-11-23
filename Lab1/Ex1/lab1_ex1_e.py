5aa22edc75dc952253e6f1580ce93962342da6fe
ArashDaneshvar <ao.daneshvar@gmail.com>
2023-11-23 16:34:03 +0100

    change parser argument and modify the name of these argument in other parts of the code, defien block_size variable

--- Lab1/Ex1/lab1_ex1_e.py
+++ Lab1/Ex1/lab1_ex1_e.py
@@ -17,24 +17,25 @@ device = 0
 # channels = 1
 # dtype = "int32"
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--sample_rate",type="int", default=48000, help="Sampling Rate in Hertz")
-parser.add_argument("--channels",type="int", default=1, help="Number of Channels")
-parser.add_argument("--dtype",type="str", default="int32", help="Resolution (str)")
-parser.add_argument("--block_size",type="int", default=48000, help="File duration in seconds (int)")
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--resolution",type=str, default="int32", help="Resolution (str)")
+parser.add_argument("--samplerate",type=int, default=48000, help="Sampling Rate in Hertz")
+parser.add_argument("--channels",type=int, default=1, help="Number of Channels")
+parser.add_argument("--duration",type=int, default=1, help="File duration in seconds(int)")
 args = parser.parse_args()
 
 def callback(indata, frames, callback_time, status):
     global audio_store
     if audio_store == True:
         timestamp = time()
-        wavfile.write(f"{timestamp}.wav", args.sample_rate, indata)
+        wavfile.write(f"{timestamp}.wav", args.samplerate, indata)
         print(timestamp)
 
 audio_store = True
+block_size = args.duration * args.samplerate
 
-with sd.InputStream(samplerate=args.sample_rate, blocksize=args.block_size, device=device, channels=args.channels, dtype=args.dtype, callback=callback):
+with sd.InputStream(samplerate=args.samplerate, blocksize=block_size, device=device, channels=args.channels, dtype=args.duration, callback=callback):
     print("Start recording")
     while True:
         key = input()
