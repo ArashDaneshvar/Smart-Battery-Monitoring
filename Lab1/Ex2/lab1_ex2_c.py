bdf999dea554b169486600b532ddaedd2778ff5c
ArashDaneshvar <ao.daneshvar@gmail.com>
2023-11-24 09:07:41 +0100

    change create ts and complete the exercise

--- Lab1/Ex2/lab1_ex2_c.py
+++ Lab1/Ex2/lab1_ex2_c.py
@@ -1,4 +1,11 @@
-import redis 
+# Modify the script to store the battery level and power plugged flag to Redis Cloud.
+
+import redis
+import psutil
+import time
+import uuid
+from datetime import datetime 
+
 
 REDIS_HOST = "redis-14980.c300.eu-central-1-1.ec2.cloud.redislabs.com"
 REDIS_PORT = 14980
@@ -12,9 +19,39 @@ redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, username=REDIS_USER
 is_connect = redis_client.ping()
 print(is_connect)
 
+#Crerate timeseries in Redis
+ts = redis_client.ts()
+mac_address = hex(uuid.getnode())
 
-written = redis_client.set("massage", value="Hello World!")
-print("written massage", written)
+try:
+    ts.create(f"{mac_address}:battery")
+except redis.ResponseError:
+    pass
 
-msg = redis_client.get("massage")
-print(msg.decode())
+try:
+    ts.create(f"{mac_address}:power")
+except redis.ResponseError:
+    pass
+
+
+
+
+status = psutil.sensors_battery()
+
+while True:
+    timestamp = time.time()
+    battery_level = status.percent
+    power_is_plugged = status.power_plugged
+    date_time_format = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S%f')
+
+    print(f'{date_time_format} - {mac_address}:{battery_level}')
+    print(f'{date_time_format} - {mac_address}:{power_is_plugged}')
+    
+    ts.add(f"{mac_address}:battery", timestamp, battery_level)
+    ts.add(f"{mac_address}:power", timestamp, power_is_plugged)
+
+    print(ts.get("mac_address"))
+    time.sleep(2)
+
+
+    
