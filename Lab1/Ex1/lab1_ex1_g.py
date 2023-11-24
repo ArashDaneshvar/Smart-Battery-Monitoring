36c4f5c5998481bf806f55a294b9a4397f73e802
ArashDaneshvar <ao.daneshvar@gmail.com>
2023-11-24 09:47:32 +0100

    remove file Lab1/Ex1/lab1_ex1_g.py

--- Lab1/Ex1/lab1_ex1_g.py
+++
@@ -1,2 +0,0 @@
-# Run the script with different parameters’ values. Try different resolutions (int16, int32), sampling
-# rates (16 kHz, 44.1 kHz and 48 kHz) and channels (1 and 2, if supported by your microphone).
